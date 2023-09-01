cfc254301fe9c7cc1314747c1e6b01ecea6e01b5
TheDesertedSea <745468443@qq.com>
2023-08-31 20:11:06 -0400

    Add Renderer Interface

--- typescript/src/renderer.ts
+++ typescript/src/renderer.ts
@@ -1,12 +1,16 @@
+interface Renderer {
+    draw: () => void,
+    setShape: (newShape: Shape) => void,
+}
 
-function newRenderer(shape: Shape) {
+
+function newRenderer(shape: Shape): Renderer {
     return {
         draw() {
             const area: number = shape.computeArea()
             console.log("Shape drawn\n" + "Its area is " + area)
         },
-        setShape(newShape: Shape)
-        {
+        setShape(newShape: Shape) {
             shape = newShape;
         }
     }
